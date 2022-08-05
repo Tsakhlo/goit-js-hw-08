aa11152a4da85f6e479291b033443d0bf3a5af72
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-05 21:59:32 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,16 +5,3 @@ import 'simplelightbox/dist/simple-lightbox.min.css'
 // Change code below this line
 
 console.log(galleryItems);
-
-const gallery = document.querySelector(".gallery");
-
-const galleryElements = galleryItems
-    .map((galleryIt) =>
-        `<a class="gallery__link" href=${galleryIt.original}> 
-                <img class="gallery__image" src =${galleryIt.preview} data-source=${galleryIt.original} alt="${galleryIt.description}"/>
-            </a>`)
-    .join("");
-
-gallery.insertAdjacentHTML("beforeend", galleryElements);
-
-const lightbox = new SimpleLightbox(".gallery__link", { captionSelector: "img", captionsData: "alt", captionPosition: "bottom", captionDelay: 250 });
