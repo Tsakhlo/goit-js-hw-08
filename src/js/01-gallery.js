b0e8f40d98bf904c340ae9b7a65726ed7d3d45b2
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-05 21:49:17 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -11,10 +11,10 @@ const gallery = document.querySelector(".gallery");
 const galleryElements = galleryItems
     .map((galleryIt) =>
         `<a class="gallery__link" href=${galleryIt.original}> 
-                <img class="gallery__image" src =${galleryIt.preview} alt="${galleryIt.description}"/>
-        </a>
-        `).join("");
+                <img class="gallery__image" src =${galleryIt.preview} data-source=${galleryIt.original} alt="${galleryIt.description}"/>
+            </a>`)
+    .join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryElements);
-  
+
 const lightbox = new SimpleLightbox(".gallery__link", { captionSelector: "img", captionsData: "alt", captionPosition: "bottom", captionDelay: 250 });
