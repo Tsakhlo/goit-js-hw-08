524b31b54a4130989e60cc569fe2bf8cca07753c
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-05 21:40:49 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css'
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = galleryItems
+    .map((galleryIt) =>
+        `<a class="gallery__link" href=${galleryIt.original}> 
+                <img class="gallery__image" src =${galleryIt.preview} alt="${galleryIt.description}"/>
+        </a>
+        `).join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryElements);
+  
+const lightbox = new SimpleLightbox(".gallery__link", { captionSelector: "img", captionsData: "alt", captionPosition: "bottom", captionDelay: 250 });
