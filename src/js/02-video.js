00d1b9827731b0531bea1c48c85ccfac4d5ec010
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-08 18:40:51 +0300

    add player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function(data) {
+    let time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+};
+
+let currentTime = localStorage.getItem("videoplayer-current-time")
+
+player.on("timeupdate", throttle(onPlay, 1000));
+       player.setCurrentTime(currentTime).then(function(seconds) {
+   
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
